17b1d468360b7939110e3a616be511b502ab29c6
Matěj Grabovský <mgrabovsky@yahoo.com>
2013-08-04 10:07:39 +0200

    Convert tabs to 4 spaces

--- codepad.py
+++ codepad.py
@@ -1,53 +1,54 @@
+#!/usr/bin/python
 import sys, argparse
 import requests
 
 languages = [
-	('C',          []),
-	('C++',        ['cpp']),
-	('D',          []),
-	('Haskell',    ['hs']),
-	('Lua',        []),
-	('OCaml',      ['oc']),
-	('PHP',        []),
-	('Perl',       ['pl']),
-	('Plain Text', ['text', 'txt']),
-	('Python',     ['py']),
-	('Ruby',       ['rb']),
-	('Scheme',     ['scm']),
-	('Tcl',        [])
+    ('C',          []),
+    ('C++',        ['cpp']),
+    ('D',          []),
+    ('Haskell',    ['hs']),
+    ('Lua',        []),
+    ('OCaml',      ['oc']),
+    ('PHP',        []),
+    ('Perl',       ['pl']),
+    ('Plain Text', ['text', 'txt']),
+    ('Python',     ['py']),
+    ('Ruby',       ['rb']),
+    ('Scheme',     ['scm']),
+    ('Tcl',        [])
 ]
 
 parser = argparse.ArgumentParser(description='submits arbitrary chunk(s) of code to'
-	' codepad.org')
+    ' codepad.org')
 parser.add_argument('files', metavar='file', nargs='*', type=argparse.FileType('r'),
-	default=[sys.stdin], help='names of files to paste or - to read from STDIN')
+    default=[sys.stdin], help='names of files to paste or - to read from STDIN')
 parser.add_argument('-l', '--language', default='text',
-	help='which syntax highlight to use (default: plain text)')
+    help='which syntax highlight to use (default: plain text)')
 parser.add_argument('-p', '--private', action='store_true', default='',
-	help='store as a private paste')
+    help='store as a private paste')
 parser.add_argument('-r', '--run', action='store_true', default='',
-	help='run the paste code after submitting')
+    help='run the paste code after submitting')
 
 args = parser.parse_args()
 
 # Find what language to send to the server or default to plain text
 language = args.language.lower()
 for syn in languages:
-	if language == syn[0].lower() or language in syn[1]:
-		language = syn[0]
-		break
+    if language == syn[0].lower() or language in syn[1]:
+        language = syn[0]
+        break
 else:
-	language = 'Plain Text'
+    language = 'Plain Text'
 
 for f in args.files:
-	payload = {
-		'code': f.read(),
-		'lang': language,
-		'private': str(args.private),
-		'run': str(args.run),
-		'submit': 'Submit'
-	}
+    payload = {
+        'code': f.read(),
+        'lang': language,
+        'private': str(args.private),
+        'run': str(args.run),
+        'submit': 'Submit'
+    }
 
-	r = requests.post('http://codepad.org/', data=payload)
-	print r.url
+    r = requests.post('http://codepad.org/', data=payload)
+    print r.url
 
